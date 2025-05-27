2e82736fba8c1ec73b5b636d3a4ba350ada9c24c
Ghabriel <ghabriel@imdigital.com>
2025-05-27 17:10:42 -0300

    Add Enum para dificuldades

--- lib/domain/Pergunta.dart
+++ lib/domain/Pergunta.dart
@@ -4,13 +4,25 @@ import 'package:bonfire/base/game_component.dart';
 import 'package:jogo_tabuleiro/domain/MapTile.dart';
 import 'package:jogo_tabuleiro/game.dart';
 
+enum Dificuldade {
+  facil,
+  medio,
+  dificil
+}
+
 class Pergunta extends GameComponent{
 
   late String pergunta;
+  late List<String> alternativas;
+  late int indexSolucao;
   late String solucao;
+  late Dificuldade dificuldade;
 
   Pergunta({
     required this.pergunta,
+    required this.alternativas,
+    required this.indexSolucao,
     required this.solucao,
+    required this.dificuldade,
   });
 }
