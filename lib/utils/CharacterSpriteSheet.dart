6276c9f046f3926c4c6ed5eaea9319c4a6790a20
Ghabriel <ghabriel@imdigital.com>
2025-05-29 09:28:02 -0300

    Add Todas as animações de movimentação do personagem

--- lib/utils/CharacterSpriteSheet.dart
+++ lib/utils/CharacterSpriteSheet.dart
@@ -10,10 +10,15 @@ class CharacterSpriteSheet{
     return SimpleDirectionAnimation(
         idleRight: getIdleRight,
         runRight: getRunRight,
+        runUp: getRunUp,
+        idleUp: getIdleUp,
+        runDown: getRunDown,
+        idleDown: getIdleDown
 
     );
   }
 
+  //Movimentos para a: DIREITA (Não é obrigatório fazer para a esquerda, pois a Engine espelha o movimento para a direita)
   Future<SpriteAnimation> get getRunRight => SpriteAnimation.load(
     path,
     SpriteAnimationData.sequenced(
@@ -26,12 +31,55 @@ class CharacterSpriteSheet{
   Future<SpriteAnimation> get getIdleRight => SpriteAnimation.load(
     path,
     SpriteAnimationData.sequenced(
-        amount: 3,
+        amount: 1,
         stepTime: 0.5,
         textureSize: Vector2.all(MapTile.tileSize),
-        texturePosition: Vector2(0,MapTile.tileSize + 5)
+        texturePosition: Vector2(MapTile.tileSize,MapTile.tileSize + 5)
     ),
   );
+
+  //Movimentos para: CIMA
+  Future<SpriteAnimation> get getIdleUp => SpriteAnimation.load(
+    path,
+    SpriteAnimationData.sequenced(
+        amount: 1,
+        stepTime: 0.5,
+        textureSize: Vector2.all(MapTile.tileSize),
+        texturePosition: Vector2(MapTile.tileSize,MapTile.tileSize + 5)
+    ),
+  );
+  Future<SpriteAnimation> get getRunUp => SpriteAnimation.load(
+    path,
+    SpriteAnimationData.sequenced(
+        amount: 3,
+        stepTime: 0.2,
+        textureSize: Vector2.all(MapTile.tileSize),
+        texturePosition: Vector2(0,3)
+    ),
+  );
+
+  //Movimentos para: BAIXO
+  Future<SpriteAnimation> get getIdleDown => SpriteAnimation.load(
+    path,
+    SpriteAnimationData.sequenced(
+        amount: 1,
+        stepTime: 0.5,
+        textureSize: Vector2.all(MapTile.tileSize),
+        texturePosition: Vector2(MapTile.tileSize,(2 * MapTile.tileSize) + 10)
+    ),
+  );
+
+  Future<SpriteAnimation> get getRunDown => SpriteAnimation.load(
+    path,
+    SpriteAnimationData.sequenced(
+        amount: 3,
+        stepTime: 0.2,
+        textureSize: Vector2.all(MapTile.tileSize),
+        texturePosition: Vector2(0, (2 * MapTile.tileSize) + 11)
+    ),
+  );
+
+
 
   static Image getRostoNeutro() {
     return Image.asset('assets/images/character/player-neutro.png',height: 100);
