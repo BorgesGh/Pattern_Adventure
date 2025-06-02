81933e7c90abbc17d5b207f5314dd3176327edf1
Ghabriel <ghabriel@imdigital.com>
2025-06-02 13:59:14 -0300

    Add atributos para pergunta

--- lib/domain/Pergunta.dart
+++ lib/domain/Pergunta.dart
@@ -3,20 +3,20 @@ import 'dart:ui';
 import 'package:bonfire/base/game_component.dart';
 import 'package:jogo_tabuleiro/domain/MapTile.dart';
 import 'package:jogo_tabuleiro/game.dart';
-
 enum Dificuldade {
   facil,
   medio,
   dificil
 }
 
-class Pergunta extends GameComponent{
-
-  late String pergunta;
-  late List<String> alternativas;
-  late int indexSolucao;
-  late String solucao;
-  late Dificuldade dificuldade;
+class Pergunta {
+  final String pergunta;
+  final List<String> alternativas;
+  final int indexSolucao;
+  final String solucao;
+  final Dificuldade dificuldade;
+  final String? headerImagem;     // Caminho opcional da imagem no header
+  final String? solucaoImagem;    // Caminho opcional da imagem na solução
 
   Pergunta({
     required this.pergunta,
@@ -24,5 +24,33 @@ class Pergunta extends GameComponent{
     required this.indexSolucao,
     required this.solucao,
     required this.dificuldade,
+    this.headerImagem,
+    this.solucaoImagem,
   });
+
+  /// Serialização para banco de dados
+  Map<String, dynamic> toMap() {
+    return {
+      'pergunta': pergunta,
+      'alternativas': alternativas.join('|'),
+      'indexSolucao': indexSolucao,
+      'solucao': solucao,
+      'dificuldade': dificuldade.index,
+      'headerImagem': headerImagem,
+      'solucaoImagem': solucaoImagem,
+    };
+  }
+
+  /// Criação a partir do banco de dados
+  factory Pergunta.fromMap(Map<String, dynamic> map) {
+    return Pergunta(
+      pergunta: map['pergunta'],
+      alternativas: (map['alternativas'] as String).split('|'),
+      indexSolucao: map['indexSolucao'],
+      solucao: map['solucao'],
+      dificuldade: Dificuldade.values[map['dificuldade']],
+      headerImagem: map['headerImagem'],
+      solucaoImagem: map['solucaoImagem'],
+    );
+  }
 }
