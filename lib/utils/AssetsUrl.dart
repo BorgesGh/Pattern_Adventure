4ef383acf5356841e8093fc591f009ccd39db4eb
Ghabriel <ghabriel@imdigital.com>
2025-06-13 08:51:56 -0300

    Add referencia aos diagramas de classe

--- lib/utils/AssetsUrl.dart
+++ lib/utils/AssetsUrl.dart
@@ -5,4 +5,26 @@ class AssetsUrl {
 
   static const String explicacao_1 = 'assets/images/explanations/explan_01.png';
   static const String explicacao_2 = 'assets/images/explanations/explan_02.png';
+
+  static const String musica_normal = 'assets/audio/Soundtrack-normal.mp3';
+  static const String musica_noite = 'assets/audio/Soundtrack-noite.mp3';
+
+  static const String effect_acertou = 'assets/audio/Som-acerto-pergunta.wav';
+  static const String effect_errou = 'assets/audio/Som-erro-pergunta.wav';
+
+  static const String som_hit_inimigo = 'assets/audio/Som-hit-inimigo.wav';
+
+  // Diagramas
+
+  static const String diagrama_abstract_factory = 'assets/images/diagramas/AbstractFactory.png';
+  static const String diagrama_adapter = 'assets/images/diagramas/Adapter.png';
+  static const String diagrama_bridge = 'assets/images/diagramas/Bridge.png';
+  static const String diagrama_decorator = 'assets/images/diagramas/Decorator.png';
+  static const String diagrama_factory = 'assets/images/diagramas/Factory.png';
+  static const String diagrama_observer = 'assets/images/diagramas/Observer.png';
+  static const String diagrama_singleton = 'assets/images/diagramas/Singleton.png';
+  static const String diagrama_state = 'assets/images/diagramas/State.png';
+  static const String diagrama_strategy = 'assets/images/diagramas/Strategy.png';
+
+
 }
